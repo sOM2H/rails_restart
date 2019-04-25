659bbac8ecb01bc07c21bc7a58b389d059309891
Mikhail Melnyk <m1kh41l.melnyk@gmail.com>
2019-04-25 16:24:30 +0300

    Move restart.sh

--- lib/rails_restart.rb
+++ lib/rails_restart.rb
@@ -3,6 +3,6 @@ require 'rails_restart/version'
 module Rails
   def self.restart(parameters = '')
     pid = IO.read('tmp/pids/server.pid')
-    system('bash restart.sh ' + pid + ' ' + parameters)
+    system('bash lib/restart.sh ' + pid + ' ' + parameters)
   end
 end
