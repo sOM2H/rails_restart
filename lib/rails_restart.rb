1a168604335004846434d11ea576ed1ead75bab7
Mikhail Melnyk <m1kh41l.melnyk@gmail.com>
2019-04-25 16:15:19 +0300

    exec -> system

--- lib/rails_restart.rb
+++ lib/rails_restart.rb
@@ -3,6 +3,6 @@ require 'rails_restart/version'
 module Rails
   def self.restart(parameters = '')
     pid = IO.read('tmp/pids/server.pid')
-    exec('bash restart.sh ' + pid + ' ' + parameters)
+    system('bash restart.sh ' + pid + ' ' + parameters)
   end
 end
