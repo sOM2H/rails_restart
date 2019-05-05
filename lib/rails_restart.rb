c1583d80ace699b5832552900290e38012f8ec16
Mikhail Melnyk <m1kh41l.melnyk@gmail.com>
2019-05-05 08:22:06 +0300

    Update logic

--- lib/rails_restart.rb
+++ lib/rails_restart.rb
@@ -3,7 +3,7 @@ require 'rails_restart/version'
 module Rails
   def self.restart(parameters = '')
     pid = IO.read('tmp/pids/server.pid')
-    cmd = '"sleep 1 && kill ' + pid + ' && rails s ' + parameters + '"'
+    cmd = '"sleep 1 && kill ' + pid + ' && sleep 5 && rails s ' + parameters + '" '
     system('echo ' + cmd + ' > restart.sh')
     system('bash restart.sh &')
   end
